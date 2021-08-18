621591511db21adcb5c92a59ab00c69def7cb09d
rodres101 <rodrigosergio93@gmail.com>
2021-08-17 21:24:52 -0400

    data types

---
+++ src/basic/function.py
@@ -0,0 +1,13 @@
+#! /Users/sergiorodrigo/Documents/tesis/test/examples/.env/bin/python
+
+def is_palindrome(word: str) -> bool:
+    i = 0 
+    j = len(word) - 1
+    while i < j:
+        if word[i] != word[j]:
+            return False
+        i += 1
+        j -= 1
+    return True
+
+print(f"apple: {is_palindrome('apple')}")
