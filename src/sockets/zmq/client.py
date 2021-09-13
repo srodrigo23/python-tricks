a8e8f847686cf0a63da8d81a3b97d31ecfb9a82c
rodres101 <rodrigosergio93@gmail.com>
2021-09-13 13:07:18 -0400

    adding some examples about selectors

--- src/sockets/zmq/client.py
+++ src/sockets/zmq/client.py
@@ -4,7 +4,7 @@
 #   Sends "Hello" to server, expects "World" back
 #
 
-import zmq
+import zmq, time
 
 context = zmq.Context()
 
@@ -15,10 +15,13 @@ socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
 #  Do 10 requests, waiting each time for a response
-for request in range(10):
+# for request in range(10):
+request = 0
+while True:
+    request += 1
     print("Sending request %s …" % request)
     socket.send(b"Hello")
-
+    time.sleep(0.2)
     #  Get the reply.
     message = socket.recv()
     print("Received reply %s [ %s ]" % (request, message))
--- src/sockets/zmq/server.py
+++ src/sockets/zmq/server.py
@@ -4,7 +4,7 @@ import zmq
 context =  zmq.Context()
 
 socket = context.socket(zmq.REP)
-socket.bind("tcp://192.168.100.8:5555")
+socket.bind("tcp://*:5555")
 
 while True:
     # wait for next request from client
@@ -12,7 +12,7 @@ while True:
     print(f"Recieved request: {message}")
 
     # Do some work
-    time.sleep(1)
+    time.sleep(0.2)
 
     # Send reply back to client
     socket.send(b"World")
